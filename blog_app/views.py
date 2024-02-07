02d362b210a6c08d683e49ec66dc91bbfd76b0aa
GlennJohansson85 <139962883+GlennJohansson85@users.noreply.github.com>
2024-02-08 00:49:40 +0100

    Update views.py
    define models :
    - home
    - post_category
    - post_details

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,3 +1,37 @@
+# blog_app/views.py
 from django.shortcuts import render
+from blog_app.models import Post, Comment
 
-# Create your views here.
+
+# Model for Main page
+def home(request):
+    posts = Post.objects.all().order_by("-created_on")
+    context = {
+        "posts": posts,
+    }
+    
+    return render(request, "blog_app/home.html", context)
+
+
+def post_category(request, category):
+    posts = Post.objects.filter(
+        categories__name__contains=category
+    ).order_by("-created_on")
+    context = {
+        "category": category,
+        "posts": posts,
+    }
+    
+    return render(request, "blog_app/post_category.html", context)
+
+
+def post_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    comments = Comment.objects.filter(post=post)
+    context = {
+        "post": post,
+        "comments": comments,
+    }
+
+    return render(request, "blog_app/post_detail.html", context)
+
