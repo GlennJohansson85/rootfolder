2a57ff47d8200757d59f742deb9fabc2295bb821
GlennJohansson85 <139962883+GlennJohansson85@users.noreply.github.com>
2024-02-08 04:09:19 +0100

    Update urls.py
    add path for post template

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -4,6 +4,7 @@ from blog_app import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path('post/', views.post, name='post'),
     path("post/<int:pk>/", views.post_detail, name="post_detail"),
     path("category/<category>/", views.post_category, name="post_category"),
 ]
