94d16a0bf58aad967ac9bb1ede00eed0600b9255
GlennJohansson85 <139962883+GlennJohansson85@users.noreply.github.com>
2024-02-08 22:21:17 +0100

    Update forms.py
    - Change order of field items
    - Change body to content
    
    - def __init__(self, *args, **kwargs) = Constructor method for the PostForm. Called when  an instance of the form is created.
    - super(PostForm, self).__init__(*args, **kwargs) = Calls the base form from the parent class.
    - self.fields['categories'].widget.attrs.update({'class': 'form-control'}) = Modefies rendering of "categories". Updates the attributes used to render this form

--- blog_app/forms.py
+++ blog_app/forms.py
@@ -4,4 +4,8 @@ from .models import Post
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['title', 'categories', 'image', 'body' ]
+        fields = ['title', 'content', 'image', 'categories']  # Include 'categories' in the fields
+
+    def __init__(self, *args, **kwargs):
+        super(PostForm, self).__init__(*args, **kwargs)
+        self.fields['categories'].widget.attrs.update({'class': 'form-control'})
