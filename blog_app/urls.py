e4bd56b1b394d78c096bc9dac2dd7d6b524d0056
GlennJohansson85 <139962883+GlennJohansson85@users.noreply.github.com>
2024-02-08 22:48:45 +0100

    Update urls.py
    - correct  home path
    - Add path to templates "user_registration", "user_login", "user logout

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -3,8 +3,11 @@ from django.urls import path
 from blog_app import views
 
 urlpatterns = [
-    path("", views.home, name="home"),
+    path('', views.home, name='home'),
     path('post/', views.post, name='post'),
     path("post/<int:pk>/", views.post_detail, name="post_detail"),
     path("category/<category>/", views.post_category, name="post_category"),
+    path('registration/', views.user_registration, name='user_registration'),
+    path('login/', views.user_login, name='user_login'),
+    path('logout/', views.user_logout, name='user_logout'),
 ]
