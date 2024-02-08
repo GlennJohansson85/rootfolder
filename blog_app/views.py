df8b238732184cd2e3696a0edf459f290fbc57f9
GlennJohansson85 <139962883+GlennJohansson85@users.noreply.github.com>
2024-02-08 23:01:58 +0100

    Update views.py
    - change order of models
    - add imports (Django Authentication and UserCreation Form), login/logut
    - add models "user_registration", "user_login" , "user_logout"
    - add decorator (@login_required) for some models.
    - add method for checks if HTTP is a post request.
    - Add instance to initialize form data from post request.
    - Add checks if form data is valid in accordance to form validation rules. If form is valid: Create new post object based on form data (does not save it immediate to database.
    - Add author association to post

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,25 +1,50 @@
 # blog_app/views.py
 from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.decorators import login_required
 from blog_app.models import Post, Comment
 from .forms import PostForm
 
 
-# home.html
-def home(request):
-    posts = Post.objects.all()
-    return render(request, "blog_app/home.html", {"posts": posts})
-
-
-def post(request):
-    if request.method == "POST":
-        form = PostForm(request.POST, request.FILES)
+def user_registration(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
+            login(request, user)
             return redirect('home')
     else:
-        form = PostForm()
+        form = UserCreationForm()
 
-    return render(request, "blog_app/post.html", {"form": form})
+    return render(request, 'blog_app/user_registration.html', {'form': form})
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = AuthenticationForm()
+
+    return render(request, 'blog_app/user_login.html', {'form': form})
+
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return redirect('home')
+
+
+# home.html
+@login_required
+def home(request):
+    # Order posts by 'created_on' in descending order
+    posts = Post.objects.all().order_by('-created_on')
+    return render(request, "blog_app/home.html", {"posts": posts})
 
 
 def post_category(request, category):
@@ -32,6 +57,22 @@ def post_category(request, category):
     }
     
     return render(request, "blog_app/post_category.html", context)
+
+
+@login_required  
+def post(request):
+    if request.method == "POST":
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            new_post = form.save(commit=False)
+            new_post.author = request.user  
+            new_post.save()
+            return redirect('home')
+    else:
+        form = PostForm()
+
+    return render(request, "blog_app/post.html", {"form": form})
+
 
 
 def post_detail(request, pk):
