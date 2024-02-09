ab8391d81370728f6d35b975ff8c83e1a85bfe6b
GlennJohansson85 <139962883+GlennJohansson85@users.noreply.github.com>
2024-02-09 02:25:26 +0100

    Update models.py
    - clean
    - add class comment: Remove post field, add author
    - Add username + date when comment

--- blog_app/models.py
+++ blog_app/models.py
@@ -2,6 +2,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class PostCategory(models.Model):
     name = models.CharField(max_length=30)
 
@@ -11,9 +12,10 @@ class PostCategory(models.Model):
     def __str__(self):
         return self.name
 
+    
 class Post(models.Model):
     title = models.CharField(max_length=255)
-    content = models.TextField()  # Add this line
+    content = models.TextField()
     image = models.ImageField(upload_to='post_images/', blank=True, null=True)
     created_on = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
@@ -23,11 +25,11 @@ class Post(models.Model):
     def __str__(self):
         return self.title
     
-class Comment(models.Model):
-    author = models.CharField(max_length=60)
-    body = models.TextField()
-    created_on = models.DateTimeField(auto_now_add=True)
-    post = models.ForeignKey("Post", on_delete=models.CASCADE)
     
+class Comment(models.Model):
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+
     def __str__(self):
-        return f"{self.author} on '{self.post}'"
+        return f"{self.author.username} - {self.created_on}"
