0131aa780bbfa1ec3cd479643d06f828101fc0ad
GlennJohansson85 <139962883+GlennJohansson85@users.noreply.github.com>
2024-02-09 02:23:42 +0100

    Update forms.py
    add commentform

--- blog_app/forms.py
+++ blog_app/forms.py
@@ -1,11 +1,21 @@
 from django import forms
-from .models import Post
+from .models import Post, Comment
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['title', 'content', 'image', 'categories']  # Include 'categories' in the fields
+        fields = ['title', 'content', 'image', 'categories']
 
     def __init__(self, *args, **kwargs):
         super(PostForm, self).__init__(*args, **kwargs)
         self.fields['categories'].widget.attrs.update({'class': 'form-control'})
+        
+        
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']
+        widgets = {
+            'text': forms.Textarea(attrs={'rows': 4, 'cols': 50}),
+        }
+        
